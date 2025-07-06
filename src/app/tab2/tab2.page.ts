b2f37f9a424d7462725ae7692a0f0922423da602
dougras <emaildedouglas@gmail.com>
2025-07-05 23:16:52 -0300

    ajuste para codigo de 6 digitos form control

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -64,7 +64,7 @@ export class Tab2Page {
   }
 
   redeemBadge() {
-    if (this.badgeCode.length === 6) {
+    if (this.otp.value.length === 6) {
       this.resgatar(this.badgeCode);
     } else {
       alert('Código inválido! Certifique-se de que possui 6 dígitos.');
